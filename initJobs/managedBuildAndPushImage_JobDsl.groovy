5b90b2e99ff5fdf2adcd72323b44c71eafb8a7c0
malibu <malibu.io@kakaoenterprise.com>
2025-03-05 16:39:42 +0900

    에러 수정.

--- initJobs/managedBuildAndPushImage_JobDsl.groovy
+++ initJobs/managedBuildAndPushImage_JobDsl.groovy
@@ -2,37 +2,31 @@ pipelineJob("kruise.managed.build-and-push-image") {
     parameters {
         stringParam {
             name("argocdApplicationName")
-            defaultValue(argocdApplicationName)
             description("kruise project 생성과정에서 argocd 에 생성된 application 이름입니다. argocd application 수정/싱크 동작을 위해 필요합니다.")
             trim(true)
         }
         stringParam {
             name("projectRepositoryUrl")
-            defaultValue(projectRepositoryUrl)
             description("빌드를 수행할 git 주소입니다.")
             trim(true)
         }
         stringParam {
             name("projectRepositoryBranch")
-            defaultValue(projectRepositoryBranch)
             description("빌드를 수행할 기본 branch 입니다.")
             trim(true)
         }
         stringParam {
             name("imagePath")
-            defaultValue(imagePath)
             description("image push 할 주소 입니다. 프로토콜을 제외한 경로 입니다. ex) domain/owner/repo")
             trim(true)
         }
         stringParam {
             name("proxy")
-            defaultValue(proxy)
             description("proxy 서버 주소입니다.")
             trim(true)
         }
         stringParam {
             name("noProxy")
-            defaultValue(noProxy)
             description("proxy 서버를 사용하지 않을 주소(도메인, ip) 목록입니다. 여러개의 주소는 ','  로 구별합니다.")
             trim(true)
         }
